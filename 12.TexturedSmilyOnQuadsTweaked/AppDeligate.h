5315202ce0a17093a576687f7ee1c58d382e66a3
Vaibhav Zodge <vaibhavzodge@Vaibhavs-MacBook-Air.local>
2018-05-29 22:43:21 +0530

    Texture smily tweaked

---
+++ 12.TexturedSmilyOnQuadsTweaked/AppDeligate.h
@@ -0,0 +1,4 @@
+
+#import "MyView.h"
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate> // protocol conformance (whom you follow). NSApplication delegates to AppDeligate
+@end
